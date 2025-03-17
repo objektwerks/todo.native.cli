46fababf5c2676e7a9633bc88c909ad0b1838eb1
objektwerks <objektwerks@runbox.com>
2025-03-17 10:32:06 -0400

    removed logger from store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -1,19 +1,17 @@
 package objektwerks
 
-import com.typesafe.scalalogging.LazyLogging
-
 import upickle.default.{read => readJson, write => writeJson}
 
 import Todo.given
 
-final class Store extends LazyLogging:
-  private val todosPath = os.home / ".todo" / "data"
+final class Store:
+  private val todosPath = os.home / ".todon"
 
   os.makeDir.all(todosPath)
-  logger.info("Initialized store.")
+  println("Initialized store.")
 
   def listTodos(): List[Todo] =
-    logger.info(s"List todos.")
+    println(s"List todos.")
     os.list(todosPath)
       .filter { path => path.baseName.nonEmpty }
       .map { path => readTodo(s"${path.baseName}.json") }
@@ -22,13 +20,13 @@ final class Store extends LazyLogging:
 
   def readTodo(file: String): Todo =
     val todoAsJson = os.read(todosPath / file)
-    logger.info(s"Read todo: $file")
+    println(s"Read todo: $file")
     readJson[Todo](todoAsJson)
 
   def writeTodo(todo: Todo): Unit =
     val todoAsJson = writeJson(todo)
     os.write.over(todosPath / todo.file, todoAsJson)
-    logger.info(s"Write todo: ${todo.id}")
+    println(s"Write todo: ${todo.id}")
 
   def nextId(): Int =
     val list = os.list(todosPath)
@@ -36,5 +34,5 @@ final class Store extends LazyLogging:
       .map { path => path.baseName.toInt }
       .toList
     val id = if list.isEmpty then 1 else list.max + 1
-    logger.info(s"Next id: $id")
+    println(s"Next id: $id")
     id
