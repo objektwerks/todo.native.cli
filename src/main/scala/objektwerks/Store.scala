8727750697d1426619fb8179af7168c006e6fa17
objektwerks <objektwerks@runbox.com>
2025-03-18 07:14:19 -0400

    added scribe to store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -8,10 +8,10 @@ final class Store:
   private val todosPath = os.home / ".todon"
 
   os.makeDir.all(todosPath)
-  println("Initialized store.")
+  scribe.info("Initialized store.")
 
   def listTodos(): List[Todo] =
-    println(s"List todos.")
+    scribe.info(s"List todos.")
     os.list(todosPath)
       .filter { path => path.baseName.nonEmpty }
       .map { path => readTodo(s"${path.baseName}.json") }
@@ -20,13 +20,13 @@ final class Store:
 
   def readTodo(file: String): Todo =
     val todoAsJson = os.read(todosPath / file)
-    println(s"Read todo: $file")
+    scribe.info(s"Read todo: $file")
     readJson[Todo](todoAsJson)
 
   def writeTodo(todo: Todo): Unit =
     val todoAsJson = writeJson(todo)
     os.write.over(todosPath / todo.file, todoAsJson)
-    println(s"Write todo: ${todo.id}")
+    scribe.info(s"Write todo: ${todo.id}")
 
   def nextId(): Int =
     val list = os.list(todosPath)
@@ -34,5 +34,5 @@ final class Store:
       .map { path => path.baseName.toInt }
       .toList
     val id = if list.isEmpty then 1 else list.max + 1
-    println(s"Next id: $id")
+    scribe.info(s"Next id: $id")
     id
